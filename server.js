579b49443a813e14e5f7493dbc9dd073005e3a07
Pornthip Mensin <pornthip@grad.nu.local>
2012-07-27 08:55:00 +0700

    remove nowjs

--- server.js
+++ server.js
@@ -13,11 +13,10 @@ app.get('/', function(req, res) {
   res.render('index');
 });
 
-app.get('/xml', function(req, res) {
+app.get('/ajax/loadxml', function(req, res) {
     var parser = new xml2js.Parser();
     parser.addListener('end', function(result) {
 	res.json(result);
-	console.log('Done.');
     });
     fs.readFile(__dirname + '/test/foo.xml', function(err, data) {
 	parser.parseString(data);
@@ -25,16 +24,18 @@ app.get('/xml', function(req, res) {
 });
 
 var server = app.listen(3000);
-var everyone = require("now").initialize(server,{socketio:{transports:['xhr-polling','jsonp-polling']}});
+
+//var everyone = require("now").initialize(server,{socketio:{transports:['xhr-polling','jsonp-polling']}});
 //var everyone = require("now").initialize(server);
 
+/*
 everyone.now.loadXML = function(callback) {
   var parser = new xml2js.Parser();
   parser.addListener('end', function(result) {
     callback(result);
-    console.log('Done.');
   });
   fs.readFile(__dirname + '/test/foo.xml', function(err, data) {
     parser.parseString(data);
   });
 };
+*/
--- static/js/app.js
+++ static/js/app.js
@@ -2,19 +2,45 @@ XMLM = Ember.Application.create({
   name:"XML Manipulation"
 });
 
+XMLM.Attribute = Ember.Object.extend({
+  key:null,
+  value:null
+});
+
 XMLM.Element = Ember.Object.extend({
-  name:null
+  name:null,
+  attribute:[]
+});
+
+XMLM.xmlController = Ember.Object.create({
+  content: XMLM.Element.create({name:"Phongphun"}),
+  load: function() {
+    $.getJSON('ajax/loadxml', function(data) {
+      $.each(data, function(key, value) {
+        console.log(key); 
+      });
+      XMLM.xmlController.content.set('name',JSON.stringify(data));
+    });
+  }
 });
 
 XMLM.elementController = Ember.Object.create({
   content: XMLM.Element.create({name:"Phongphun"}),
-  say: function() {
-    alert("Hello");
+  load: function() {
+    $.getJSON('ajax/loadxml', function(data) {
+      XMLM.elementController.content.set('name',JSON.stringify(data));
+    });
   }
 });
 
+XMLM.xmlController.load();
+
+
+/*
 now.ready(function() {
   now.loadXML(function(result) {
-    alert(JSON.stringify(result));
+    // alert(JSON.stringify(result));
+    XMLM.elementController.content.set('name','PK');
   });
 });
+*/
